31bd0ce8d909b66087f37fb5e508dd3ee36028f3
Garland Kan <garland@managedkube.com>
2023-07-29 00:46:11 +0000

    Refactored to a python script.  easier to parse json than bash

--- recreation.gov/run.sh
+++ recreation.gov/run.sh
@@ -7,8 +7,76 @@ TOUR_ID=$2
 YEAR=$3
 MONTH=$4
 
+if $DEBUG == "true"; then
+    curl -s "https://www.recreation.gov/api/ticket/availability/facility/${FACILITY_ID}/monthlyAvailabilitySummaryView?year=${YEAR}&month=${MONTH}&inventoryBucket=FIT&tourId=${TOUR_ID}" | jq .
+fi
 
-# Curl the URL.  Then pipe the output to jq to parse the JSON starting with the .facility_availability_summary_view_by_local_date key.  For each of the dates, look at the `tour_availability_summary_view_by_tour_id` key and for each key under there look for the `reservable` key.  If it's greater than 0, then print the local_date and reservable value.
-AVAILABLE_DATES=$(curl -s "https://www.recreation.gov/api/ticket/availability/facility/${FACILITY_ID}/monthlyAvailabilitySummaryView?year=${YEAR}&month=${MONTH}&inventoryBucket=FIT&tourId=${TOUR_ID}" | jq -r '.facility_availability_summary_view_by_local_date | to_entries[] | select(.value.tour_availability_summary_view_by_tour_id | to_entries[].value.reservable > 0) | .key + "," + (.value.tour_availability_summary_view_by_tour_id | to_entries[].value.reservable | tostring)')
+
+# Curl the URL.  Then pipe the output to jq to parse the JSON starting with the .facility_availability_summary_view_by_local_date key.  For each of the dates, look at the `tour_availability_summary_view_by_tour_id` key and for each key under there look for the `reservable` key.  If it's greater than 0, then print the local_date value.
+AVAILABLE_DATES=$(curl -s "https://www.recreation.gov/api/ticket/availability/facility/${FACILITY_ID}/monthlyAvailabilitySummaryView?year=${YEAR}&month=${MONTH}&inventoryBucket=FIT&tourId=${TOUR_ID}" | jq -r '.facility_availability_summary_view_by_local_date | to_entries[] | select(.value.tour_availability_summary_view_by_tour_id | to_entries[].value.reservable > 0) | .key')
+
+
+#AVAILABLE_DATES=$(curl -s "https://www.recreation.gov/api/ticket/availability/facility/${FACILITY_ID}/monthlyAvailabilitySummaryView?year=${YEAR}&month=${MONTH}&inventoryBucket=FIT&tourId=${TOUR_ID}" | jq -r '.facility_availability_summary_view_by_local_date | to_entries[] | select(.value.tour_availability_summary_view_by_tour_id | to_entries[].value.reservable > 0) | .key + "," + (.value.tour_availability_summary_view_by_tour_id | to_entries[].value.reservable | tostring)')
+#AVAILABLE_DATES=$(curl -s "https://www.recreation.gov/api/ticket/availability/facility/${FACILITY_ID}/monthlyAvailabilitySummaryView?year=${YEAR}&month=${MONTH}&inventoryBucket=FIT&tourId=${TOUR_ID}" | jq -r '.facility_availability_summary_view_by_local_date | to_entries[] | select(.value.tour_availability_summary_view_by_tour_id | to_entries[].value.reservable > 0) | .key + "," + (.value.tour_availability_summary_view_by_tour_id | to_entries[].value.reservable | tostring)')
+
 
 echo "$AVAILABLE_DATES"
+
+
+
+# Create a loop that will loop through the $AVAILABLE_DATES variable
+for DATE in $AVAILABLE_DATES; do
+    # Print the date
+    echo "Date: $DATE"
+
+    tour_time=$(curl -s "https://www.recreation.gov/api/ticket/availability/facility/${FACILITY_ID}?date=${DATE}" | jq -r '.[] | .tour_time | tostring')
+    inventory_count=$(curl -s "https://www.recreation.gov/api/ticket/availability/facility/${FACILITY_ID}?date=${DATE}" | jq -r '.[] | .inventory_count.ANY | tostring')
+    reservation_count=$(curl -s "https://www.recreation.gov/api/ticket/availability/facility/${FACILITY_ID}?date=${DATE}" | jq -r '.[] | .reservation_count.ANY | tostring')
+
+    # Create a variable that will curl https://www.recreation.gov/api/ticket/availability/facility/251853?date=$DATE which will return a JSON object.  Pipe that to jq to parse the JSON starting with the first array position [0] and Look for the inventory_count.ANY value and reservation_count.ANY value and if inventory_count.ANY minus reservation_count.ANY is greater than 0, then print the time slot.  
+    
+    #AVAILABLE_TIMES=$(curl -s "https://www.recreation.gov/api/ticket/availability/facility/${FACILITY_ID}?date=${DATE}" | jq -r '.[] | to_entries[] | select(.value.inventory_count.ANY - .value.reservation_count.ANY > 0) | .key')
+    
+    # AVAILABLE_TIMES=$(curl -s "https://www.recreation.gov/api/ticket/availability/facility/${FACILITY_ID}?date=${DATE}" | jq -r '.timeslots | to_entries[] | select(.value.availability > 0) | .key + "," + (.value.availability | tostring)')
+
+    # echo $AVAILABLE_TIMES
+
+    echo "Tour Time: $tour_time"
+    echo "Inventory Count: $inventory_count"
+    echo "Reservation Count: $reservation_count"
+
+    # exit 0
+
+    count=0
+    while IFS= read -r line; do
+        echo "... $line ..."
+        echo "inventory_count - reservation_count: $((inventory_count[$count] - reservation_count[$count]))"
+        count=$((count+1))
+
+        exit 0
+    done <<< "$tour_time"
+
+
+    # count=0
+    # for i in "${tour_time}"; do
+    #     echo "tour_time: $i"
+    #     # echo the $count possition in $inventory_count variable
+    #     # echo "inventory_count: ${inventory_count[$count]}"
+
+    #     # inventory_count minus reservation_count
+    #     # echo "inventory_count - reservation_count: $((inventory_count[$count] - reservation_count[$count]))"
+
+
+    #     count=$((count+1))
+
+    #     exit 0
+    # done
+
+    exit 0
+
+
+    #AVAILABLE_TIMES=$(curl -s "https://www.recreation.gov/api/ticket/availability/facility/${FACILITY_ID}?date=${DATE}" | jq -r '.timeslots | to_entries[] | select(.value.availability > 0) | .key + "," + (.value.availability | tostring)')
+    # AVAILABLE_TIMES=$(curl -s "https://www.recreation.gov/api/ticket/availability/facility/251853?date=$DATE" | jq -r '.timeslots | to_entries[] | select(.value.availability > 0) | .key + "," + (.value.availability | tostring)')
+
+done
+
---
+++ recreation.gov/run_python.py
@@ -0,0 +1,94 @@
+# python script that takes in 4 user inputs via the command line input: FACILITY_ID, TOUR_ID, YEAR, MONTH. Then http get this url: https://www.recreation.gov/api/ticket/availability/facility/${FACILITY_ID}/monthlyAvailabilitySummaryView?year=${YEAR}&month=${MONTH}&inventoryBucket=FIT&tourId=${TOUR_ID}. It will return a json which we will parse in a loop
+
+import requests
+import sys
+import os
+
+## Get user inputs
+FACILITY_ID = sys.argv[1]
+TOUR_ID = sys.argv[2]
+YEAR = sys.argv[3]
+MONTH = sys.argv[4]
+
+# How many tickets are you looking for per time slot
+NUMBER_OF_RESERVABLE_PER_TIME_SLOT = 2
+
+## Get envars
+debug_on = os.environ.get('DEBUG_ON')
+
+## Construct URL with user inputs
+url = f"https://www.recreation.gov/api/ticket/availability/facility/{FACILITY_ID}/monthlyAvailabilitySummaryView" #?year={YEAR}&month={MONTH}&inventoryBucket=FIT&tourId={TOUR_ID}"
+
+print("URL: " + url + "\n")
+
+## Make HTTP GET request and get JSON data
+## Set the header or it will think it is a bot and respond with some error
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+}
+response = requests.get(url, headers=headers, params={'year': YEAR, 'month': MONTH, 'inventoryBucket': 'FIT', 'tourId': TOUR_ID})
+data = response.json()
+
+if debug_on == 'true':
+    print("XXXXXXXXXXXXXXXXXXXX")
+    print(response.content)
+    print(f"URL: {url}?year={YEAR}&month={MONTH}&inventoryBucket=FIT&tourId={TOUR_ID}\n")
+    print("XXXXXXXXXXXXXXXXXXXX")
+
+## Loop through JSON data on the `facility_availability_summary_view_by_local_date` key
+for facility_availability_summary_view_by_local_date in data:
+    print(facility_availability_summary_view_by_local_date)
+
+    try:
+        # For each item in facility_availability_summary_view_by_local_date that starts and the key starts with a number loop through it
+        for a_date in data.get(facility_availability_summary_view_by_local_date):
+            print(a_date)
+
+            # print(data.get(facility_availability_summary_view_by_local_date).get(a_date).get('tour_availability_summary_view_by_tour_id').get(TOUR_ID).get('reservable'))
+
+            # THis is the total reservable slots for the day.  It could be 1 slot free for the 9:00am time slot and 1 slot free for the 10:00am time slot.  So it is 2 reservable slots for the day
+            reservable = data.get(facility_availability_summary_view_by_local_date).get(a_date).get('tour_availability_summary_view_by_tour_id').get(TOUR_ID).get('reservable')
+
+            # We need query each day which will give us the time slots for that day and then we can
+            # calculate how many slots are available for each time slot
+            if reservable > NUMBER_OF_RESERVABLE_PER_TIME_SLOT:
+                print(f"reservable: {reservable}\n")
+
+                #############################################
+                # Lets look at the time slots in each day
+                #############################################
+                daily_timeslot_url = f"https://www.recreation.gov/api/ticket/availability/facility/{FACILITY_ID}"
+                daily_timeslot_response = requests.get(daily_timeslot_url, headers=headers, params={'date': a_date})
+                daily_timeslot_data = daily_timeslot_response.json()
+
+                if debug_on == 'true':
+                    print("XXXXXXXXXXXXXXXXXXXX")
+                    print(daily_timeslot_response.content)
+                    print(f"URL: {daily_timeslot_url}?date={a_date}\n")
+                    print("XXXXXXXXXXXXXXXXXXXX")
+
+                for a_daily_timeslot in daily_timeslot_data:
+                    print("yyyyyyyy")
+
+                    tour_time = a_daily_timeslot.get('tour_time')
+                    reservation_count = a_daily_timeslot.get('reservation_count').get('ANY')
+                    inventory_count = a_daily_timeslot.get('inventory_count').get('ANY')
+
+                    print(f"tour_time: {tour_time}")
+                    print(f"reservation_count: {reservation_count}")
+                    print(f"inventory_count: {inventory_count}")
+
+                    if (inventory_count - reservation_count) > NUMBER_OF_RESERVABLE_PER_TIME_SLOT:
+                        print(f"inventory_count - reservation_count: {inventory_count - reservation_count}\n")
+
+                        # There are free slots available that is over the NUMBER_OF_RESERVABLE_PER_TIME_SLOT
+                        # Send this information to Slack
+
+                        
+
+
+    except AttributeError:
+        print("AttributeError: 'NoneType' object has no attribute 'get'")
+        break
+
+

b51646ca3f1c1d977136ce43797ffa8529ada6d5
garland <garland.kan@anthem.com>
2024-02-07 14:12:21 -0800

    houseseat v2.1 - 1 hour loop

--- houseseats.com/notify-on-event/search.py
+++ houseseats.com/notify-on-event/search.py
@@ -8,6 +8,7 @@ import urllib.parse
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 import yaml
+import time
 
 # How many tickets are you looking for per time slot
 NUMBER_OF_RESERVABLE_PER_TIME_SLOT = 2
@@ -28,111 +29,124 @@ def send_to_slack(message):
 
     response = client.chat_postMessage(channel=SLACK_CHANNEL, text=message)
 
+## Run the main program in a loop because Github Actions' scheduler can only
+## run a job once every 5 minutes and most of the time it is like 10-15 mins
+## each interval, which is too long for us.  So we will run this in a loop and
+## restart the program every 60 mins.
+loop_count = 0
+while True:
 
-print('Starting program...')
-
-
-# Open the state YAML file
-with open('state.yaml', 'r') as file:
-    # Load the YAML data from the file
-    previous_state = yaml.safe_load(file)
-
-# Now data is a list of dictionaries
-print(previous_state)
-
-new_state = []
-
-# Create a session object
-s = requests.Session()
-
-# Define the login data
-login_data = {
-    'email': os.environ['HOUSESEAT_USERNAME'],
-    'password': os.environ['HOUSESEAT_PASSWORD'],
-    'submit': 'login',
-    'lastplace': '',
-}
-
-headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-            'Accept-Language': 'en-US,en;q=0.5' 
-        }
-
-# URL of the login page
-login_url = 'https://lv.houseseats.com/member/index.bv'
-
-# Send a POST request to the login page with your login data
-s.post(login_url, data=login_data, headers=headers)
-
-# Now s maintains the 'logged in' session. You can use it to send additional requests
-response = s.get('https://lv.houseseats.com/member/ajax/upcoming-shows.bv?supersecret=&search=&sortField=&startMonthYear=&endMonthYear=&startDate=&endDate=&start=0', headers=headers)
-
-# Print the content of the 'protected page'
-# print(response.text)
+    print('Starting program...')
 
 
-# Find all instances of the <h1> title of a show
-pattern = r'<h1><a href="./tickets/view/\?showid=(.*?)">(.*?)</a></h1>'
+    # Open the state YAML file
+    with open('state.yaml', 'r') as file:
+        # Load the YAML data from the file
+        previous_state = yaml.safe_load(file)
 
-# Use re.findall() to find all instances of the pattern in the string
-matches = re.findall(pattern, response.text.lower())
+    # Now data is a list of dictionaries
+    print(previous_state)
 
-## Loop through the list of matches which should be all of the shows/events on the page
-for event in matches:
-    print(f'found: {event[0]} | {event[1]}')
+    new_state = []
 
-    ## Compare to see if the event is in the exclude list.
-    ## If it is in the exclude list, then do not send a message to Slack
-    ## if it is not in the exclude list, then send a message to Slack
-    found_exclude = False
+    # Create a session object
+    s = requests.Session()
 
-    with open('items_to_exclude.txt', 'r') as file:
-        for line in file:
-            ## Do something with each line
-            search_item = line.strip()
-            # print(search_item)
+    # Define the login data
+    login_data = {
+        'email': os.environ['HOUSESEAT_USERNAME'],
+        'password': os.environ['HOUSESEAT_PASSWORD'],
+        'submit': 'login',
+        'lastplace': '',
+    }
 
-            # Define regular expression pattern
-            pattern = fr'{search_item}'
+    headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+                'Accept-Language': 'en-US,en;q=0.5' 
+            }
 
-            # Use regular expression to extract text
-            result = re.search(pattern, event[1].lower())
+    # URL of the login page
+    login_url = 'https://lv.houseseats.com/member/index.bv'
 
-            # Print extracted text
-            if result:
-                print(f'found {search_item}')
+    # Send a POST request to the login page with your login data
+    s.post(login_url, data=login_data, headers=headers)
 
-                found_exclude = True
+    # Now s maintains the 'logged in' session. You can use it to send additional requests
+    response = s.get('https://lv.houseseats.com/member/ajax/upcoming-shows.bv?supersecret=&search=&sortField=&startMonthYear=&endMonthYear=&startDate=&endDate=&start=0', headers=headers)
 
-    ## If the event is not in the exclude list, then continue to process the event
-    if found_exclude == False:
-        print(f'[NOT IN EXCLUDE LIST] not found in exclude list: {event[0]} | {event[1]}')
+    # Print the content of the 'protected page'
+    # print(response.text)
 
-        ## Add the event to the new state dictionary list
-        new_state.append({
-            'event_id': event[0],
-            'event_name': event[1]
-        })
 
-        ## If the event is NOT in the previous state dictionary list then send a message to Slack
-        ## and write it to the new state dictionary list
-        found_in_previous_state = False
+    # Find all instances of the <h1> title of a show
+    pattern = r'<h1><a href="./tickets/view/\?showid=(.*?)">(.*?)</a></h1>'
 
-        for item in previous_state:
-            if item['event_id'] == event[0]:
-                print(f'found in previous state: {event[0]} | {event[1]}')
-                found_in_previous_state = True
+    # Use re.findall() to find all instances of the pattern in the string
+    matches = re.findall(pattern, response.text.lower())
 
-        if not found_in_previous_state:
-            ## Send message to Slack
-            print(f'[NOTIFY] Sending slack message for: {event[0]} | {event[1]}')
-            if slack_enabled == 'true':
-                send_to_slack(f"""
-                    *[Houseseat v2]* Search found: <https://lv.houseseats.com/member/tickets/view/?showid={urllib.parse.unquote(event[0])}|{urllib.parse.unquote(event[1])}>  
-                """)
+    ## Loop through the list of matches which should be all of the shows/events on the page
+    for event in matches:
+        print(f'found: {event[0]} | {event[1]}')
 
-# Output the new state dictionary list to the state.yaml file
-with open('state.yaml', 'w') as file:
-    # Write the YAML data to the file
-    yaml.dump(new_state, file)
+        ## Compare to see if the event is in the exclude list.
+        ## If it is in the exclude list, then do not send a message to Slack
+        ## if it is not in the exclude list, then send a message to Slack
+        found_exclude = False
+
+        with open('items_to_exclude.txt', 'r') as file:
+            for line in file:
+                ## Do something with each line
+                search_item = line.strip()
+                # print(search_item)
+
+                # Define regular expression pattern
+                pattern = fr'{search_item}'
+
+                # Use regular expression to extract text
+                result = re.search(pattern, event[1].lower())
+
+                # Print extracted text
+                if result:
+                    print(f'found {search_item}')
+
+                    found_exclude = True
+
+        ## If the event is not in the exclude list, then continue to process the event
+        if found_exclude == False:
+            print(f'[NOT IN EXCLUDE LIST] not found in exclude list: {event[0]} | {event[1]}')
+
+            ## Add the event to the new state dictionary list
+            new_state.append({
+                'event_id': event[0],
+                'event_name': event[1]
+            })
+
+            ## If the event is NOT in the previous state dictionary list then send a message to Slack
+            ## and write it to the new state dictionary list
+            found_in_previous_state = False
+
+            for item in previous_state:
+                if item['event_id'] == event[0]:
+                    print(f'[found in previous state] {event[0]} | {event[1]}')
+                    found_in_previous_state = True
+
+            if not found_in_previous_state:
+                ## Send message to Slack
+                print(f'[NOTIFY] Sending slack message for: {event[0]} | {event[1]}')
+                if slack_enabled == 'true':
+                    send_to_slack(f"""
+                        *[Houseseat v2.1]* Search found: <https://lv.houseseats.com/member/tickets/view/?showid={urllib.parse.unquote(event[0])}|{urllib.parse.unquote(event[1])}>  
+                    """)
+
+    # Output the new state dictionary list to the state.yaml file
+    with open('state.yaml', 'w') as file:
+        # Write the YAML data to the file
+        yaml.dump(new_state, file)
+
+    print('Sleeping for 60 seconds...')
+    loop_count += 1
+    if loop_count >= 60:
+        print('Restarting program...')
+        break
+    time.sleep(60)

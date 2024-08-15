dd2e3b7640b93c4e88f346292dd33fc9c530da9f
garland <garland.kan@anthem.com>
2024-08-15 10:02:15 -0700

    nexus3

--- nexus-global-entry/trusted-traveler-scheduler/trusted-traveler-scheduler/src/schedule_retriever.py
+++ nexus-global-entry/trusted-traveler-scheduler/trusted-traveler-scheduler/src/schedule_retriever.py
@@ -165,9 +165,11 @@ class ScheduleRetriever:
 
                     ## GarDebug
                     foo = datetime.strptime(appointment["startTimestamp"], "%Y-%m-%dT%H:%M").isoformat()
+                    print(f"GarDebug: the appointment datetime stamp foo: {foo}")
                     date_string1 = datetime.strftime(appointment["startTimestamp"], '%a, %B %d, %Y')
+                    print(f"GarDebug: the appointment datetime stamp date_string1: {date_string1}")
                     date_string2 = datetime.strftime(appointment.appointment_date, '%a, %B %d, %Y')
-                    print(f"GarDebug: the appointment datetime stamp: {foo} | {date_string1} | {date_string2}")
+                    print(f"GarDebug: the appointment datetime stamp date_string2: {date_string2}")
                     sys.exit()
 
                     all_active_appointments.append(datetime.strptime(appointment["startTimestamp"], "%Y-%m-%dT%H:%M").isoformat())

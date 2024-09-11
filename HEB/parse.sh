c97c1376f0c261584fb429956579e808736ca5d0
garland <garland.kan@anthem.com>
2024-09-11 11:24:00 -0700

    HEB

--- HEB/parse.sh
+++ HEB/parse.sh
@@ -5,12 +5,16 @@ file="out.txt"
 
 line_count=0
 
+one_line=""
+
 while read line; do
   line_count=$((line_count + 1))
 
-  echo "$line"
+#   echo "$line"
+  one_line="$one_line,$line"
 
   if (( line_count % 6 == 0 )); then
-    echo "-----"
+    echo $one_line
+    one_line=""
   fi
 done < "$file"
